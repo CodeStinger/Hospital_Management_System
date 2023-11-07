8524d8df93395c2bf5e9c693c25400f0f3c906ac
CodeStinger <vivekv.chavan.03@gmail.com>
2023-11-07 11:07:12 +0530

    view appointment module completed

--- Hospital_Management_System/src/main/java/com/dao/appointmentDAO.java
+++ Hospital_Management_System/src/main/java/com/dao/appointmentDAO.java
@@ -80,4 +80,39 @@ public class appointmentDAO {
 		
 		return list;
 	}
+	
+public List<Appointment> getAllAppointmentsOfDoctor(int doctorid){
+		
+		List<Appointment> list= new ArrayList<>();
+		
+		Appointment ap = null;
+		
+		try {
+			String sql = "SELECT * FROM appointment WHERE doctorid=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, doctorid);
+			ResultSet rs = ps.executeQuery();
+		
+			while(rs.next()) {
+				ap = new Appointment();
+				ap.setId(rs.getInt(1));
+				ap.setUserid(rs.getInt(2));
+				ap.setFullname(rs.getString(3));
+				ap.setGender(rs.getString(4));
+				ap.setAge(rs.getInt(5));
+				ap.setDate(rs.getString(6));
+				ap.setEmail(rs.getString(7));
+				ap.setPhone(rs.getString(8));
+				ap.setDisease(rs.getString(9));
+				ap.setAddress(rs.getString(10));
+				ap.setStatus(rs.getString(11));
+				ap.setDoctorid(rs.getInt(12));
+				list.add(ap);
+							}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
 }
